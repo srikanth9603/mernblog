1488b80e49fed004bed9650a08131c1de9f8df5b
srikanth <sekarsrikanth266@gmail.com>
2025-02-28 18:27:10 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        requireed: true,
+        required: true,
         unique: true,
     },
     email:{
         type: String,
-        requireed: true,
+        required: true,
         unique: true,
     },
     password:{
         type: String,
-        requireed: true,
+        required: true,
         
     },
     }, {timestamps: true}
 );
 
-const user = mangoose.model('user,userSchema');
+const User = mongoose.model('User',userSchema);
 
-export default user;
+export default User;
